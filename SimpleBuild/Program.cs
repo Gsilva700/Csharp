242dfc29437cb859937660cb87c1fd68e0a3ba78
Gabriel Silva <79112647+Gsilva700@users.noreply.github.com>
2025-02-19 14:14:02 -0300

    Add files via upload
    If you wanna use some of the loose files on this repo, i suggested that you download this simple build and copy and paste into the file program.cs on this folder.

---
+++ SimpleBuild/Program.cs
@@ -0,0 +1,25 @@
+﻿/* A finalidade desse código é inverter uma cadeia de caracteres
+ e contar o número de vezes que um caractere específico é exibido. 
+*/
+
+
+string str = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+
+int x = 0;
+
+foreach (char i in charMessage) 
+
+   { if (i == 'o') 
+   
+   { x++; } 
+   
+   }
+
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+
+Console.WriteLine($"'o' appears {x} times.");
